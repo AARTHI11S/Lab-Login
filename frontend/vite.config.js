27bad5a98eba0c1a70f6b2d50a0aa0feacb607a7
AARTHI S <ssaarthi11@gmail.com>
2024-12-13 22:23:41 +0530

    find the errr

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Use @ for shorter imports
+    },
+  },
+  build: {
+    outDir: 'publish', // Output directory for the build files
+  },
+});
