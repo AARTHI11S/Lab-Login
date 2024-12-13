dbbbade5adc19005ce733a4cf98bb01520ab1069
AARTHI S <ssaarthi11@gmail.com>
2024-12-13 22:01:57 +0530

    find the err

--- backend/index.js
+++ backend/index.js
@@ -8,20 +8,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(
-    "mongodb+srv://ssaarthi11:9JpqlH2jqUSGOW1g@cluster0.m0lrh.mongodb.net/",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
-
+// mongoose
+//   .connect(
+//     "mongodb+srv://ssaarthi11:9JpqlH2jqUSGOW1g@cluster0.m0lrh.mongodb.net/",
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     }
+//   )
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.log(err));
+  mongoose
+  .connect("mongodb+srv://ssaarthi11:9JpqlH2jqUSGOW1g@cluster0.m0lrh.mongodb.net/") // Simplified connection without deprecated options
+  .then(() => {
+    console.log("MongoDB connection successful...");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
 app.use("/api/students", studentRoutes);
 app.use("/api/users", UserRoutes);
 
+app.get('/', (req, res) => {
+  res.send("Hello world!");
+});
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
